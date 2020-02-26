82a6ba556d293ccff51d32006f075f05e4641705
Thiago Victorino <>
2020-02-25 23:51:55 -0300

    wip: Add parse url with some tests

--- src/UrlParser.php
+++ src/UrlParser.php
@@ -3,9 +3,173 @@
 namespace thiagovictorino\ResourceExporter;
 
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Http\Request;
+use thiagovictorino\ResourceExporter\Exceptions\UrlParserException;
+use function GuzzleHttp\Psr7\str;
+
 class UrlParser
 {
-    public function load(string $url): array {
+    protected $url;
 
+    protected $bearerToken;
+
+    protected $httpClient;
+
+    protected $delay = 0;
+
+    protected $bootstrapThree = false;
+
+    protected $request;
+
+    protected $result;
+
+    public function __construct()
+    {
+        $this->httpClient = resolve(Client::class);
+        $this->result = collect();
+    }
+
+    public function endpoint(string $url): UrlParser
+    {
+
+        if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+            throw new UrlParserException('The url '.$url.' is invalid');
+        }
+
+        $this->url = $url;
+
+        $this->createRequest();
+
+        return $this;
+    }
+
+    protected function createRequest()
+    {
+        $url_components = parse_url($this->url);
+        parse_str($url_components['query'], $query);
+        $this->request = Request::create($this->url, 'GET', $query);
+    }
+
+    public function withBearerToken(string $token): UrlParser
+    {
+
+        $this->bearerToken = $token;
+
+        return $this;
+    }
+
+    public function withBootstrapThree(): UrlParser
+    {
+        $this->bootstrapThree = true;
+        return $this;
+    }
+
+    public function withDelay(int $seconds): UrlParser
+    {
+        $this->delay = $seconds;
+        return $this;
+    }
+
+    public function load(): iterable
+    {
+
+        $this->validateBeforeLoad();
+
+        $this->sendRequest();
+
+        return $this->result;
+    }
+
+    protected function sendRequest()
+    {
+        try {
+
+            $response = $this->httpClient->request('GET', $this->url, [
+              'headers' => $this->setHeaders()
+            ]);
+
+            $next_url = $this->setResult($response->getBody()->getContents());
+
+            if(!empty($next_url)){
+                $this->url = $next_url;
+                sleep($this->delay);
+                $this->sendRequest();
+            }
+
+        } catch (RequestException $e) {
+
+            if ($e->hasResponse()) {
+                throw new UrlParserException($e->getMessage().' - Response:'.str($e->getResponse()),$e->getCode(), $e);
+            }
+
+            throw new UrlParserException($e->getMessage().' - Request:'.str($e->getRequest()),$e->getCode(), $e);
+
+        } catch (\Exception $e) {
+
+            throw new UrlParserException($e->getMessage(),$e->getCode(), $e);
+
+        }
+
+    }
+
+    protected function setHeaders()
+    {
+        return array_merge(
+          [
+            'Accept' => 'application/json'
+          ],
+          $this->setAuthHeaders()
+        );
+    }
+
+    protected function setAuthHeaders(): array
+    {
+
+        if(!empty($this->bearerToken)){
+            return ['Authorization' => 'Bearer ' . $this->bearerToken];
+        }
+
+        return [];
+    }
+
+    private function validateBeforeLoad()
+    {
+        if (empty($this->url)) {
+            throw new UrlParserException('You must inform an URL at least before call load()');
+        }
+    }
+
+    private function setResult(string $contents) : ?string
+    {
+        $json = json_decode($contents);
+
+        if($this->bootstrapThree){
+            return $this->parseBootstrapThreeResult($json);
+        }
+
+        $this->result->push($json->data);
+
+        if(empty($json->next_page_url)) {
+            return '';
+        }
+
+        return $json->next_page_url.'&'.urldecode(http_build_query($this->request->except('page')));
+    }
+
+    private function parseBootstrapThreeResult($json)
+    {
+
+        if (!isset($json->meta)) {
+           throw new UrlParserException('This is not a Bootstrap 3 format');
+        }
+        $this->result->push($json->data);
+
+        if(empty($json->meta->pagination->links->next)) {
+            return '';
+        }
+
+        return $json->meta->pagination->links->next.'&'.urldecode(http_build_query($this->request->except('page')));
     }
 }
--- tests/Feature/UrlParserTest.php
+++ tests/Feature/UrlParserTest.php
@@ -3,16 +3,274 @@
 
 namespace thiagovictorino\ResourceExporter\Tests\Feature;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Foundation\Auth\User;
+use thiagovictorino\ResourceExporter\Exceptions\UrlParserException;
 use thiagovictorino\ResourceExporter\Tests\TestCase;
+use thiagovictorino\ResourceExporter\UrlParser;
+use function GuzzleHttp\Psr7\stream_for;
 
 class UrlParserTest extends TestCase
 {
     /**
      * @test
      */
+    public function it_will_throws_an_url_exception() {
+
+        $this->expectException(UrlParserException::class);
+
+        $urlParser = resolve(UrlParser::class);
+
+        $urlParser->endpoint('xxxxxxx')
+          ->load();
+    }
+    /**
+     * @test
+     */
     public function it_will_parse_an_resource_paginated() {
-        $users = factory(User::class, 30)->make();
-        dd(User::where(1,1)->paginate(10));
+
+        $guzzle = \Mockery::mock(Client::class);
+          $guzzle->shouldReceive('request')->withArgs([
+          'GET',
+          "http://localhost:8082/test?sort=-id&filter[bookable_type]=properties",
+          [
+            'headers' => [
+              'Accept' => 'application/json'
+            ]
+          ]
+        ])->once()->andReturn(
+          new Response(
+            200,
+            [],
+            '{
+                  "current_page": 1,
+                  "data": [{
+                    "id": 1,
+                    "username": "testeroot",
+                    "email": "teste@test.com",
+                    "email_verified_at": "2020-01-29 17:31:26",
+                    "created_at": "2020-01-29 17:31:18",
+                    "updated_at": "2020-01-29 17:31:18",
+                    "block": 0,
+                    "terms_validation": 0,
+                    "validation_ip": null,
+                    "validated_at": null,
+                    "last_login": null,
+                    "reengaged_at": null,
+                    "person_id": 1
+                  }],
+                  "first_page_url": "http:\/\/localhost:8082\/test?page=1",
+                  "from": 1,
+                  "last_page": 2,
+                  "last_page_url": "http:\/\/localhost:8082\/test?page=2",
+                  "next_page_url": "http:\/\/localhost:8082\/test?page=2",
+                  "path": "http:\/\/localhost:8082\/test",
+                  "per_page": 1,
+                  "prev_page_url": null,
+                  "to": 1,
+                  "total": 2
+                }'
+            )
+          );
+
+        $guzzle->shouldReceive('request')->withArgs([
+          'GET',
+          'http://localhost:8082/test?page=2&sort=-id&filter[bookable_type]=properties',
+          [
+            'headers' => [
+              'Accept' => 'application/json'
+            ]
+          ]
+        ])->once()->andReturn(
+          new Response(
+            200,
+            [],
+            '{
+                      "current_page": 2,
+                      "data": [{
+                        "id": 1,
+                        "username": "testeroot",
+                        "email": "teste@test.com",
+                        "email_verified_at": "2020-01-29 17:31:26",
+                        "created_at": "2020-01-29 17:31:18",
+                        "updated_at": "2020-01-29 17:31:18",
+                        "block": 0,
+                        "terms_validation": 0,
+                        "validation_ip": null,
+                        "validated_at": null,
+                        "last_login": null,
+                        "reengaged_at": null,
+                        "person_id": 1
+                      }],
+                      "first_page_url": "http:\/\/localhost:8082\/test?page=1",
+                      "from": 2,
+                      "last_page": 2,
+                      "last_page_url": "http:\/\/localhost:8082\/test?page=2",
+                      "next_page_url": "",
+                      "path": "http:\/\/localhost:8082\/test",
+                      "per_page": 1,
+                      "prev_page_url": null,
+                      "to": 1,
+                      "total": 2
+                    }'));
+
+        $this->instance(Client::class, $guzzle);
+
+        $urlParser = resolve(UrlParser::class);
+
+        $result = $urlParser->endpoint('http://localhost:8082/test?sort=-id&filter[bookable_type]=properties')
+          ->load();
+
+        $this->assertCount(2, $result);
+
+        $first_page = $result->first();
+
+        $this->assertObjectHasAttribute('id', $first_page[0]);
+        $this->assertObjectHasAttribute('username', $first_page[0]);
+        $this->assertObjectHasAttribute('email', $first_page[0]);
+        $this->assertObjectHasAttribute('person_id', $first_page[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_throws_an_exception_when_parse_bootstrap_three() {
+
+        $this->expectException(UrlParserException::class);
+        $guzzle = \Mockery::mock(Client::class);
+        $guzzle->shouldReceive('request')->withArgs([
+          'GET',
+          "http://localhost:8082/test?sort=-id&filter[bookable_type]=properties",
+          [
+            'headers' => [
+              'Accept' => 'application/json'
+            ]
+          ]
+        ])->once()->andReturn(new Response(200, [],'{"current_page": 1,"data":[{"id":1,"username":"testeroot","email":"teste@test.com","email_verified_at":"2020-01-29 17:31:26","created_at":"2020-01-29 17:31:18","updated_at":"2020-01-29 17:31:18","block":0,"terms_validation":0,"validation_ip":null,"validated_at":null,"last_login":null,"reengaged_at":null,"person_id":1}],"first_page_url":"http:\/\/localhost:8082\/test?page=1","from":1,"last_page":2,"last_page_url":"http:\/\/localhost:8082\/test?page=2","next_page_url":"http:\/\/localhost:8082\/test?page=2","path":"http:\/\/localhost:8082\/test","per_page":1,"prev_page_url":null,"to":1,"total":2}'));
+
+
+        $this->instance(Client::class, $guzzle);
+
+        $urlParser = resolve(UrlParser::class);
+
+        $urlParser->endpoint('http://localhost:8082/test?sort=-id&filter[bookable_type]=properties')
+          ->withBootstrapThree()
+          ->load();
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_parse_an_resource_paginated_with_bootstrap_three() {
+
+        $guzzle = \Mockery::mock(Client::class);
+        $guzzle->shouldReceive('request')->withArgs([
+          'GET',
+          "http://localhost:8082/test?sort=-id&filter[bookable_type]=properties",
+          [
+            'headers' => [
+              'Accept' => 'application/json'
+            ]
+          ]
+        ])->once()->andReturn(new Response(200, [],'{
+                  "current_page": 1,
+                  "data": [{
+                    "id": 1,
+                    "username": "testeroot",
+                    "email": "teste@test.com",
+                    "email_verified_at": "2020-01-29 17:31:26",
+                    "created_at": "2020-01-29 17:31:18",
+                    "updated_at": "2020-01-29 17:31:18",
+                    "block": 0,
+                    "terms_validation": 0,
+                    "validation_ip": null,
+                    "validated_at": null,
+                    "last_login": null,
+                    "reengaged_at": null,
+                    "person_id": 1
+                  }],
+                  "meta": {
+                    "pagination": {
+                      "first_page_url": "http:\/\/localhost:8082\/test?page=1",
+                      "from": 2,
+                      "last_page": 2,
+                      "last_page_url": "http:\/\/localhost:8082\/test?page=2",
+                      "links": {
+                        "next" : "http:\/\/localhost:8082\/test?page=2"
+                      },
+                      "next_page_url": "http:\/\/localhost:8082\/test?page=2",
+                      "path": "http:\/\/localhost:8082\/test",
+                      "per_page": 1,
+                      "prev_page_url": null,
+                      "to": 1,
+                      "total": 2
+                    }
+                  }
+                }'));
+
+        $guzzle->shouldReceive('request')->withArgs([
+          'GET',
+          'http://localhost:8082/test?page=2&sort=-id&filter[bookable_type]=properties',
+          [
+            'headers' => [
+              'Accept' => 'application/json'
+            ]
+          ]
+        ])->once()->andReturn(new Response(200,
+            [],
+            '{
+                  "current_page": 2,
+                  "data": [{
+                    "id": 2,
+                    "username": "testeroot",
+                    "email": "teste@test.com",
+                    "email_verified_at": "2020-01-29 17:31:26",
+                    "created_at": "2020-01-29 17:31:18",
+                    "updated_at": "2020-01-29 17:31:18",
+                    "block": 0,
+                    "terms_validation": 0,
+                    "validation_ip": null,
+                    "validated_at": null,
+                    "last_login": null,
+                    "reengaged_at": null,
+                    "person_id": 1
+                  }],
+                  "meta": {
+                    "pagination": {
+                      "first_page_url": "http:\/\/localhost:8082\/test?page=1",
+                      "from": 2,
+                      "last_page": 2,
+                      "last_page_url": "http:\/\/localhost:8082\/test?page=2",
+                      "next_page_url": "",
+                      "links": {
+                        "next" : ""
+                      },
+                      "path": "http:\/\/localhost:8082\/test",
+                      "per_page": 1,
+                      "prev_page_url": null,
+                      "to": 1,
+                      "total": 2
+                    }
+                  }
+                }')
+        );
+
+        $this->instance(Client::class, $guzzle);
+
+        $urlParser = resolve(UrlParser::class);
+
+        $result = $urlParser->endpoint('http://localhost:8082/test?sort=-id&filter[bookable_type]=properties')
+          ->withBootstrapThree()
+          ->load();
+
+        $this->assertCount(2, $result);
+
+        $first_page = $result->first();
+
+        $this->assertObjectHasAttribute('id', $first_page[0]);
+        $this->assertObjectHasAttribute('username', $first_page[0]);
+        $this->assertObjectHasAttribute('email', $first_page[0]);
+        $this->assertObjectHasAttribute('person_id', $first_page[0]);
     }
 }
